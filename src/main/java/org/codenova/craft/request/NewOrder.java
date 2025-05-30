b923388c3eafbcd95c72a33bccf77f0402ae627c
rin <krin02@naver.com>
2025-05-30 10:05:55 +0900

    feat : 주문 및 재고 관리 엔티티, 리포지토리, 컨트롤러 구현

---
+++ src/main/java/org/codenova/craft/request/NewOrder.java
@@ -0,0 +1,22 @@
+package org.codenova.craft.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Setter
+@Getter
+public class NewOrder {
+
+    @Setter
+    @Getter
+    public static class Item {
+        private String productId;
+        private int quantity;
+    }
+
+    private LocalDate dueDate;
+    private List<Item> items;
+}
